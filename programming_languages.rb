d9b76706286582726b43fa003b7787de8137ff00
Marco Miranda <marmirand@hotmail.com>
2019-04-12 12:05:32 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
-  # your code here
+  #new_hash = {}
+  languages.each do |k, v|
+    #binding.pry
+    v.each do |key, value|
+      new_hash[key] = value
+      new_hash[key][:style] = []
+      new_hash[key][:style]<<k
+      #binding.pry
+     end
+  end
+  new_hash[:javascript][:style]<<:oo
+  new_hash
+  #binding.pry
 end
