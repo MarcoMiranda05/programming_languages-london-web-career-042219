4739ee1dc0cddc83278e4fe22cdf09094a35aad1
Marco Miranda <marmirand@hotmail.com>
2019-04-12 12:06:39 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,20 @@
 def reformat_languages(languages)
-  #new_hash = {}
-  languages.each do |k, v|
-    #binding.pry
-    v.each do |key, value|
-      new_hash[key] = value
-      new_hash[key][:style] = []
-      new_hash[key][:style]<<k
-      #binding.pry
-     end
-  end
-  new_hash[:javascript][:style]<<:oo
-  new_hash
   #binding.pry
+    new_langs = {}
+
+     languages.each do | l_style, l_langs |
+      # we loop once for each style
+      l_langs.each do | langname, langdata |
+        # add language and style
+        if new_langs[langname] == nil
+          new_langs[langname] = { style: [] }
+        end
+        new_langs[langname][:style] << l_style
+        new_langs[langname][:type] = langdata[:type]
+      end
+    end
+
+     #binding.pry
+
+     new_langs
 end
